2c426708ea3da5dd45cda55e43dc62e8d00e03e3
Alex Horsman <aninhumer@gmail.com>
2011-08-14 11:35:57 +0100

    Added decoding support to BData with a Reader

--- BData.hs
+++ BData.hs
@@ -8,50 +8,80 @@ module Data.BData (
     bDataPack ) where
 
 import Data.BEncode
-import Data.ByteString.Lazy.Char8 (ByteString, pack)
-import Data.Map hiding (map)
+import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.Map as M
 import Control.Monad.Writer
+import Control.Monad.Reader
 
 class BData a where
-    bWrap :: a -> BEncode
+    bWrap   :: a -> BEncode
+    bUnwrap :: BEncode -> Maybe a
 
 instance BData BEncode where
     bWrap = id
+    bUnwrap = Just . id
 
-instance BData ByteString where
+instance BData BS.ByteString where
     bWrap = BString
+    bUnwrap (BString a) = Just a
+    bUnwrap _ = Nothing
 
 instance BData String where
-    bWrap = BString . pack
+    bWrap = BString . BS.pack
+    bUnwrap (BString a) = Just $ BS.unpack a
+    bUnwrap _ = Nothing
 
 instance BData Integer where
     bWrap = BInt
+    bUnwrap (BInt a) = Just a
+    bUnwrap _ = Nothing
 
 instance BData Int where
     bWrap = BInt . toInteger
+    bUnwrap (BInt a) = Just $ fromInteger a
+    bUnwrap _ = Nothing
 
 instance BData [BEncode] where
     bWrap = BList
+    bUnwrap (BList a) = Just a
+    bUnwrap _ = Nothing
 
-instance BData (Map String BEncode) where
+instance BData (M.Map String BEncode) where
     bWrap = BDict
+    bUnwrap (BDict a) = Just a
+    bUnwrap _ = Nothing
 
 type BDictW = Writer [(String, BEncode)] ()
 
 instance BData BDictW where
-    bWrap = BDict . fromList . execWriter
+    bWrap = BDict . M.fromList . execWriter
+    bUnwrap _ = Nothing
 
 bEntry :: (BData a) => String -> a -> BDictW
 bEntry k v = writer ((), [(k, bWrap v)])
 
 instance BData BListW where
     bWrap = BList . execWriter
+    bUnwrap _ = Nothing
 
 type BListW = Writer [BEncode] ()
 
 bAppend :: (BData a) => a -> BListW
 bAppend v = writer ((), [bWrap v])
 
-bDataPack :: (BData a) => a -> ByteString
+bDataPack :: (BData a) => a -> BS.ByteString
 bDataPack = bPack . bWrap
+
+type BDictR a = Reader BEncode (Maybe a)
+
+bGetEntry :: (BData a) => String -> BDictR a
+bGetEntry k = reader extract
+    where extract b =
+              do dict <- bUnwrap b
+                 entry <- M.lookup k dict
+                 bUnwrap entry
+
+withBDecode :: BS.ByteString -> BDictR a -> Maybe a
+withBDecode body reader =
+    bRead body >>= runReader reader 
 
